5b15420c1e5c0da91ec53214b6fa73634bb2398e
BaoQiang <mailbaoqiang@gmail.com>
2017-07-03 12:20:15 +0800

    down sixfoot info

--- doutula/scrapy/spiders/sixfoot.py
+++ doutula/scrapy/spiders/sixfoot.py
@@ -8,17 +8,20 @@
 @time: 2017/7/3 10:53
 """
 
-import random
-import time
 import json
+import time
 
+import requests
 import scrapy
 from scrapy import Request
+from xml.etree import ElementTree as et
+import traceback
 
 from doutula.pth import FILE_PATH
 from doutula.scrapy.items import SixFootItem
 
 out_file = '{}/sixfoot.json'.format(FILE_PATH)
+out_file2 = '{}/sixfoot_new.json'.format(FILE_PATH)
 
 root_url = 'http://foooooot.com'
 
@@ -37,12 +40,14 @@ class SixFootSpider(scrapy.Spider):
 
         res_list = []
         for item in json_data['data']:
-            if item['pic_footprint_count'] >= 100:
+            if item['pic_footprint_count'] >= 50:
                 six = SixFootItem()
                 six['id'] = item['id']
                 six['name'] = item['name']
                 six['activity'] = item['activity']
                 six['voteup'] = item['pic_footprint_count']
+                six['latitude'] = item['latitude']
+                six['lngitude'] = item['lngitude']
 
                 res_list.append(six)
 
@@ -52,12 +57,51 @@ class SixFootSpider(scrapy.Spider):
             for item in res_list:
                 fw.write('{}\n'.format(item))
 
-        yield Request(search_url_fmt.format(next_ts), callback=self.parse_item)
+                # yield Request(search_url_fmt.format(next_ts), callback=self.parse_item)
+
+
+def run():
+    google_apis_fmt = 'http://maps.google.com/maps/api/geocode/xml?latlng={},{}&language=zh-CN&sensor=false'
+
+    with open(out_file, 'r', encoding='utf-8') as f, \
+            open(out_file2, 'w', encoding='utf-8') as fw:
+        for line in f:
+            line = line.strip()
+
+            json_data = json.loads(line)
+            try:
+                url = google_apis_fmt.format(json_data['latitude'], json_data['lngitude'])
+                response = requests.get(url, proxies={
+                    'http': 'http:127.0.0.1:8123',
+                    'https': 'http:127.0.0.1:8123'
+                })
+                json_data['loc'] = parse_res(response)
+            except Exception as e:
+                traceback.print_exc()
+
+            json.dump(json_data, fw, ensure_ascii=False, sort_keys=True)
+            fw.write('\n')
+
+            break
+
+
+def parse_res(res):
+    doc = et.fromstring(res.content.decode())
+    return doc.find('result')[1].text
+
+
+def test():
+    # print(random.random())
+    # print(time.time())
+
+    # 2010 - 03 - 03 14: 38:24
+    st = time.localtime(1267598304)
+    print(time.strftime('%Y-%m-%d %H:%M:%S', st))
 
 
 def main():
-    print(random.random())
-    print(time.time())
+    # test()
+    run()
 
 
 if __name__ == '__main__':
